88e69a2d298a7f928eea52a0ccc73bbf90a43664
justinchen673 <justinchenchen673@gmail.com>
2018-09-04 10:37:31 -0400

    Fix the multiple highlighted tabs bug for the main 4 tabs

--- content.js
+++ content.js
@@ -1,2 +1,18 @@
+// Unify the header across all the pages
 var h1 = document.getElementsByTagName("h1");
 h1[0].innerHTML = "Rensselaer's Student Information System";
+
+// Fix the issue that causes multiple highlighted tabs
+var tabon = document.getElementsByClassName("tabon");
+var bgtabon = document.getElementsByClassName("bgtabon");
+if (document.title == "Main Menu" || window.location.href == "https://sis.rpi.edu/rss/twbkwbis.P_GenMenu?name=amenu.P_AcctInfoMnu") {
+    tabon[0].classList.add('taboff');
+    tabon[0].classList.remove('tabon');
+    bgtabon[0].classList.add('bgtaboff');
+    bgtabon[0].classList.remove('bgtabon');
+} else if (document.title == "Personal Information Menu" || document.title == "Student Menu") {
+    tabon[1].classList.add('taboff');
+    tabon[1].classList.remove('tabon');
+    bgtabon[1].classList.add('bgtaboff');
+    bgtabon[1].classList.remove('bgtabon');
+}
