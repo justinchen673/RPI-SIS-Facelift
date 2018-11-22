187c84776cc87e0abd7a8e9d329102709a4ec713
justinchen673 <justinchenchen673@gmail.com>
2018-11-22 15:37:03 -0500

    Move to accountDetail

--- js/accountDetail/summaryBySemester.js
+++ js/accountDetail/summaryBySemester.js
@@ -3,7 +3,7 @@ if (window.location.href == "https://sis.rpi.edu/rss/bwskoacc.P_ViewAcct") {
     createPageTitle("Account Summary by Semester", "infotextdiv", 'c');
 
     // Adjustments to the paragraph above the table
-    document.getElementsByClassName("infotexttable")[0].innerHTML = "<BLOCKQUOTE><strong>This is your account summary by term. Pending third party contract payments, financial aid, and memos are NOT included in the summary.</strong></BLOCKQUOTE>";
+    document.getElementsByClassName("infotexttable")[0].innerHTML = "This is your account summary by term. Pending third party contract payments, financial aid, and memos are NOT included in the summary.";
 
     addTableTitle(0, "5", "Summary");
 
