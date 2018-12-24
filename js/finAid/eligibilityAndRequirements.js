721f869f4fa2b78f9f168a7386309c77704065a6
Justin Chen <justinchenchen673@gmail.com>
2018-12-24 01:42:56 -0800

    added eligibility and reqs

---
+++ js/finAid/eligibilityAndRequirements.js
@@ -0,0 +1,6 @@
+if (window.location.href == "https://sis.rpi.edu/rss/bwrkelig.P_DispEligReq") {
+    // Create page title, need to use a custom insert
+    createPageTitle("Eligibility & Requirements", "form", 'f');
+
+
+}
